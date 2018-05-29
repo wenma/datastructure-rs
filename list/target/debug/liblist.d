d6ce72376e852c8000d130b3bb7215267d196395
wen.ma <wen.ma@qunar.com>
2018-05-29 18:23:08 +0800

    update

--- list/target/debug/liblist.d
+++
@@ -1,1 +0,0 @@
-/home/q/datastructure-rs/list/target/debug/liblist.rlib: /home/q/datastructure-rs/list/src/lib.rs /home/q/datastructure-rs/list/src/first.rs
--- list/target/debug/list-a84eb902dbd35b90.d
+++
@@ -1,1 +0,0 @@
-/home/q/datastructure-rs/list/target/debug/list-a84eb902dbd35b90: /home/q/datastructure-rs/list/src/lib.rs /home/q/datastructure-rs/list/src/first.rs
