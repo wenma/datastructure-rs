76e0ce63cd52049745d4f414b6caa25c3f1d7499
wen.ma <wen.ma@qunar.com>
2018-05-29 18:21:02 +0800

    first commit

---
+++ list/target/debug/liblist.d
@@ -0,0 +1,1 @@
+/home/q/datastructure-rs/list/target/debug/liblist.rlib: /home/q/datastructure-rs/list/src/lib.rs /home/q/datastructure-rs/list/src/first.rs
---
+++ list/target/debug/list-a84eb902dbd35b90.d
@@ -0,0 +1,1 @@
+/home/q/datastructure-rs/list/target/debug/list-a84eb902dbd35b90: /home/q/datastructure-rs/list/src/lib.rs /home/q/datastructure-rs/list/src/first.rs
