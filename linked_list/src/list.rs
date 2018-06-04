db38fbe30b64d7fec924a394510b55e3dde5e26f
wen.ma <wen.ma@qunar.com>
2018-06-04 23:17:56 +0800

    update

--- linked_list/src/list.rs
+++ linked_list/src/list.rs
@@ -1,52 +1,51 @@
 
 
-
+use std::rc::Rc;
 
 #[derive(Debug)]
 pub struct List {
-	head: Link
+    head: Rc<Link>
 }
+
 
 #[derive(Debug)]
 enum Link {
-	Empty,
-	More(Box<Node>)
+    Empty,
+    More(Box<Node>)
 }
 
 #[derive(Debug)]
 struct Node {
-	data: i32,
-	next: Link
+    data: i32,
+    next: Rc<Link>
 }
 
 
 impl List {
-	pub fn new() -> Self {
-		List {
-			head: Link::Empty
-		}
-	}
+    pub fn new() -> Self {
+        List {
+            head: Rc::new(Link::Empty)
+        }
+    }
 
-	pub fn insert_head(&mut self, elem: i32) {
+    pub fn insert_head(&mut self, elem: i32) {
 
-		match self.head {
-			Link::Empty => {
-				self.head = Link::More(Box::new(Node {
-					data: elem,
-					next: Link::Empty
-				}));
-			},
-			Link::More(a) => {
-				self.head = Link::More(Box::new(Node {
-					data: elem,
-					next: Link::More(a)
-				}));
-			}
-		}
-		
-	}
+        if (Rc::get_mut(&mut self.head)).is_some() {
+            self.head = Rc::new(Link::More(Box::new(Node {
+                    data: elem,
+                    next: self.head.clone()
+                })));
 
-	pub fn insert_tail(&mut self, elem: i32) {
+        } else {
+            self.head = Rc::new(Link::More(Box::new(Node {
+                    data: elem,
+                    next: Rc::new(Link::Empty)
+            })));
+        }
+        
+    }
 
-	}
+    pub fn print(&self) {
+        println!("{:?}", self.head);
+    }
 }
--- linked_list/src/main.rs
+++ linked_list/src/main.rs
@@ -5,5 +5,13 @@ mod list;
 use list::List;
 
 fn main() {
-    let list: List = List::new();
+    let mut list: List = List::new();
+
+    list.insert_head(1);
+    list.insert_head(2);
+    list.insert_head(3);
+    list.insert_head(4);
+
+    list.print();
+
 }
